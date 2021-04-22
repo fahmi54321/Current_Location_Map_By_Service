0ce3bb4072fb1d4d850d628bcdb289721ac848d1
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-04-22 11:12:18 +0700

    register

--- app/src/main/java/com/android/currentlocationmapbyservice/Common.kt
+++ app/src/main/java/com/android/currentlocationmapbyservice/Common.kt
@@ -1,5 +1,9 @@
 package com.android.currentlocationmapbyservice
 
+import com.android.currentlocationmapbyservice.Model.DriverInfoModel
+
 object Common {
+    var currentUser: DriverInfoModel?=null
+    val DRIVER_INFO_REFERENCE: String = "DriverInfo"
     val DRIVER_LOCATION_REFERENCE: String = "DriverLocations"
 }
--- app/src/main/java/com/android/currentlocationmapbyservice/SplashScreenActivity.kt
+++ app/src/main/java/com/android/currentlocationmapbyservice/SplashScreenActivity.kt
@@ -1,14 +1,23 @@
 package com.android.currentlocationmapbyservice
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Toast
+import com.android.currentlocationmapbyservice.Model.DriverInfoModel
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import java.util.*
@@ -16,13 +25,18 @@ import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    //todo 2 register
+    private lateinit var database: FirebaseDatabase
+    private lateinit var driverInfoRef: DatabaseReference
+
     //todo 4 login
-    companion object{
+    companion object {
         private val LOGIN_REQUEST_CODE = 7171
     }
-    private lateinit var providers:List<AuthUI.IdpConfig>
-    private lateinit var firebaseAuth:FirebaseAuth
-    private lateinit var listener : FirebaseAuth.AuthStateListener
+
+    private lateinit var providers: List<AuthUI.IdpConfig>
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var listener: FirebaseAuth.AuthStateListener
 
     //todo 5 login
     override fun onStart() {
@@ -32,7 +46,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     //todo 6 login
     private fun delaySplashScreen() {
-        Completable.timer(3,TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+        Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
             .subscribe({
                 firebaseAuth.addAuthStateListener(listener)
             })
@@ -40,7 +54,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     //todo 7 login
     override fun onStop() {
-        if (firebaseAuth!=null && listener!=null){
+        if (firebaseAuth != null && listener != null) {
             firebaseAuth.removeAuthStateListener(listener)
         }
 
@@ -58,6 +72,11 @@ class SplashScreenActivity : AppCompatActivity() {
 
     //todo 9 login
     private fun init() {
+
+        //todo 3 register
+        database = FirebaseDatabase.getInstance()
+        driverInfoRef = database.getReference(Common.DRIVER_INFO_REFERENCE)
+
         providers = Arrays.asList(
             AuthUI.IdpConfig.PhoneBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
@@ -65,13 +84,107 @@ class SplashScreenActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         listener = FirebaseAuth.AuthStateListener {
             val user = it.currentUser
-            if (user!=null){
-                Toast.makeText(this, user.phoneNumber, Toast.LENGTH_SHORT).show()
-//                checkUserFromFirebase()
-            }else{
+            if (user != null) {
+//                Toast.makeText(this, user.phoneNumber, Toast.LENGTH_SHORT).show()
+                //todo 4 register
+                checkUserFromFirebase()
+            } else {
                 showLoginLayout()
             }
         }
+    }
+
+    //todo 5 register
+    private fun checkUserFromFirebase() {
+        driverInfoRef.child(FirebaseAuth.getInstance().currentUser?.uid ?: "")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val model = snapshot.getValue(DriverInfoModel::class.java)
+                        gotoMapsActivity(model)
+                    } else {
+                        showRegisterLayout()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@SplashScreenActivity, error.message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
+    }
+
+    //todo 6 register
+    private fun showRegisterLayout() {
+        val builder = AlertDialog.Builder(this, R.style.DialogTheme)
+        val itemView = LayoutInflater.from(this).inflate(R.layout.layout_register, null)
+        var firstName = itemView.findViewById<View>(R.id.edt_first_name) as TextInputEditText
+        var lastName = itemView.findViewById<View>(R.id.edt_last_name) as TextInputEditText
+        var phoneNumber = itemView.findViewById<View>(R.id.edit_phone_number) as TextInputEditText
+        var btnContinue = itemView.findViewById<View>(R.id.btn_continue) as Button
+        var progressBar = itemView.findViewById<View>(R.id.progress_bar) as ProgressBar
+
+        //set data
+        if (FirebaseAuth.getInstance().currentUser?.phoneNumber != null && !TextUtils.isDigitsOnly(
+                FirebaseAuth.getInstance().currentUser?.phoneNumber
+            )
+        ) {
+            phoneNumber.setText(FirebaseAuth.getInstance().currentUser?.phoneNumber)
+        }
+
+        builder.setView(itemView)
+        val dialog = builder.create()
+        dialog.show()
+
+        //event
+        btnContinue.setOnClickListener {
+            if (TextUtils.isDigitsOnly(firstName.text.toString())) {
+                Toast.makeText(this, "Please Enter First Name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (TextUtils.isDigitsOnly(lastName.text.toString())) {
+                Toast.makeText(this, "Please Enter Last Name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (TextUtils.isDigitsOnly(phoneNumber.text.toString())) {
+                Toast.makeText(this, "Please Enter Phone Number", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }else{
+
+                var model = DriverInfoModel()
+                model.firstName = firstName.text.toString()
+                model.lastName = lastName.text.toString()
+                model.phoneNumber = phoneNumber.text.toString()
+                model.rating = 0.0
+
+                driverInfoRef.child(FirebaseAuth.getInstance().currentUser?.uid?:"")
+                    .setValue(model)
+                    .addOnFailureListener{
+                        Toast.makeText(this, "" + it.message, Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                        progressBar.visibility = View.GONE
+                    }
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Register SuccessFully", Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                        progressBar.visibility = View.GONE
+                        gotoMapsActivity(model)
+                    }
+            }
+        }
+
+        //register atur rule database firebase
+//            {
+//                    "rules": {
+//                    ".read": "auth !=null",  // 2021-5-7
+//                    ".write": "auth !=null",  // 2021-5-7
+//                }
+//            }
+    }
+
+    private fun gotoMapsActivity(model: DriverInfoModel?) {
+        Common.currentUser = model
+        startActivity(Intent(this, MapsActivity::class.java))
+        finish()
     }
 
     //todo 10 login
@@ -95,12 +208,12 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == LOGIN_REQUEST_CODE){
+        if (requestCode == LOGIN_REQUEST_CODE) {
             val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK){
+            if (resultCode == Activity.RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-            }else{
-                Toast.makeText(this, ""+response?.error?.message, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "" + response?.error?.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
